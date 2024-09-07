85581860159425aab73e34d516d3b07c405e47ce
Zeph <Zeph.levin@gmail.com>
2024-09-07 07:23:09 -0400

    Added a data table

---
+++ src/stockrweb/src/components/DataTable.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+const props = defineProps(['data']);
+const headers = props.data.headers;
+const records = props.data.records;
+
+</script>
+<template>
+    <h2>This is your data: </h2>
+    <table class="table data-table">
+        <thead>
+            <tr>
+                <th v-for="(header, ix) in headers" :key="ix">
+                    {{ header }}
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(record, ix) in records" :key="ix">
+                <td v-for="(field, ix) in record.fields" :key="ix">
+                    {{ field }}
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
--- src/stockrweb/src/views/AboutView.vue
+++ src/stockrweb/src/views/AboutView.vue
@@ -1,5 +1,25 @@
+<script setup>
+import DataTable from '../components/DataTable.vue'
+</script>
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+
+    <hr>
+
+    <DataTable :data="{
+      headers: [
+        'Ticker',
+        'Last'
+      ],
+      records: [
+        {
+          fields: [
+            'MSFT',
+            100
+          ]
+        }
+      ]
+    }"></DataTable>
   </div>
 </template>
